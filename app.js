afa6ea9ae55d8a55747a19e4fefb876598e4a8f0
abdulrahman <abdelr7manfared@gmail.com>
2022-08-15 23:29:12 +0200

    refactor: Solve issues mentioned

--- app.js
+++ app.js
@@ -3,36 +3,41 @@ let cnt = 0;
 function Instructor_names(tags) {
   let str = "";
   for (let i = 0; i < tags.instructors.length; i++) {
-    str += `<h4 class="g">${tags.instructors[i].name}</h4>`;
+    str += `<h4 class="instructor-name">${tags.instructors[i].name}</h4>`;
   }
 
   return str;
 }
 function CountStars(tag) {
-  let s = `<div> <span class="checked">4.4</span>`;
+  let s = `<div> <span class="stars">4.4</span>`;
   let Rate = tag.rating;
   for (let i = 1; i <= 5; i++) {
-    if (Rate >= i) s += `<span class="fa fa-star checked"></span>`;
+    if (Rate >= i) s += `<span class="fa fa-star stars"></span>`;
     else if (Rate + 0.5 >= i)
-      s += `<span class="fa fa-star-half-full checked"></span>`;
-    else s += `<span class="fa fa-star-o checked "></span>`;
+      s += `<span class="fa fa-star-half-full stars"></span>`;
+    else s += `<span class="fa fa-star-o stars "></span>`;
   }
   s += `</div>`;
   return s;
 }
+function Course_data(tags) {
+  let str =
+    `
+<div class = "courses"> 
+<img class="course-image"src="${tags.image}"alt="pyhon"/>
+<div class = "course-data">
+<h3 class ="course-title">${tags.title}</h3>` +
+    Instructor_names(tags) +
+    CountStars(tags) +
+    `</div>
+<h5 class ="course-price">${tags.price} $</h5>
+</div>`;
+  return str;
+}
 function Create_Course(tags) {
   let str = "";
   for (let i = 0; i < tags.courses.length; i++) {
-    str +=
-      `
-      <div class = "cour1"> 
-      <img class="fl"src="${tags.courses[i].image}"alt="pyhon"/>
-      <div class = "Head_courses"><h3 class ="b">${tags.courses[i].title}</h3>` +
-      Instructor_names(tags.courses[i]) +
-      CountStars(tags.courses[i]) +
-      `</div>
-      <h5 class ="b">${tags.courses[i].price} $</h5>
-      </div>`;
+    str += Course_data(tags.courses[i]);
   }
 
   return str;
@@ -41,22 +46,11 @@ const filterCourses = async (e) => {
   e.preventDefault();
 
   let text = document.querySelector(".Search").value.toLowerCase();
-  let x = document.querySelector(".cour");
+  let x = document.querySelector(".course-info");
   let str = "";
-  console.log(tag.courses.length);
   for (let i = 0; i < tag.courses.length; i++) {
     if (tag.courses[i].title.toLowerCase().includes(text)) {
-      str +=
-        `
-        <div class = "cour1"> 
-        <img class="fl"src="${tag.courses[i].image}"alt="pyhon"/>
-        <div class = "Head_courses"><h3 class ="b">${tag.courses[i].title}</h3>` +
-        Instructor_names(tag.courses[i]) +
-        CountStars(tag.courses[i]) +
-        `</div>
-        <h5 class ="b">${tag.courses[i].price} $</h5>
-        </div>`;
-      console.log(str);
+      str += Course_data(tag.courses[i]);
     }
   }
   x.innerHTML = str;
@@ -67,23 +61,26 @@ const LoadCourses = async (p) => {
   let data = await fet.json();
   const Res = data[p][0];
   tag = data[p][0];
-  let x = document.querySelector(".course_pre");
+  let x = document.querySelector(".course-type");
   let str = `
-  <div class ="ctr">
-  <h2 class="text_about_course1" >${Res.header}</h2>
-  <p class="text_about_course2">${Res.description}</p>    
+  <div class ="course-content">
+  <h2 class="header" >${Res.header}</h2>
+  <p class="description">${Res.description}</p>    
   <button class = "explore-course">Explore ${p}</button>
-  <div class="cour">`;
+  <div class="course-info">`;
   str += Create_Course(Res);
   str += `</div></div>`;
   x.innerHTML = str;
 };
-let last = -1;
+
+// e.target = item return from get element id
+let last = document.getElementById("c_python");
+document.getElementById("c_python").style.color = "black";
 function reload_Courses(e) {
   let x = e.target.innerHTML;
-  if (last != -1) last.target.style.color = "gray";
+  if (last != -1) last.style.color = "gray";
   e.target.style.color = "black";
-  last = e;
+  last = e.target;
   LoadCourses(x);
 }
 LoadCourses("Python");
