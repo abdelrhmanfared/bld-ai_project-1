51daaa11c391c57bc49da9e45228df99faf29cab
abdulrahman <abdelr7manfared@gmail.com>
2022-08-18 03:47:09 +0200

    feat : Make responsive carousel of cards and category grid

--- app.js
+++ app.js
@@ -1,58 +1,158 @@
-let Course;
+let tag;
+let currentIndex = 0; // -1 + 1 = 0
+let pages = 5;
+let def = "Python";
 function Instructor_names(tags) {
   let str = "";
   for (let i = 0; i < tags.instructors.length; i++) {
-    str += `<h4 class="g">${tags.instructors[i].name}</h4>`;
+    str += `<h4 class="instructor-name">${tags.instructors[i].name}</h4>`;
   }
+
   return str;
 }
 function CountStars(tag) {
-  let s = `<div> <span class="checked">4.4</span>`;
+  let s = `<div> <span class="stars">4.4</span>`;
   let Rate = tag.rating;
   for (let i = 1; i <= 5; i++) {
-    if (Rate >= i) s += `<span class="fa fa-star checked"></span>`;
+    if (Rate >= i) s += `<span class="fa fa-star stars"></span>`;
     else if (Rate + 0.5 >= i)
-      s += `<span class="fa fa-star-half-full checked"></span>`;
-    else s += `<span class="fa fa-star-o checked "></span>`;
+      s += `<span class="fa fa-star-half-full stars"></span>`;
+    else s += `<span class="fa fa-star-o stars "></span>`;
   }
   s += `</div>`;
   return s;
 }
-function Create_Course(tags) {
+function Course_data(tags) {
   let str =
-    `<div class = "cour1"> <img class="fl"src="${tags.image}"alt="pyhon"/><div class = "Head_courses"><h3 class ="b">${tags.title}</h3>` +
+    `
+<div class = "courses"> 
+<img class="course-image"src="${tags.image}"alt="pyhon"/>
+<div class = "course-data">
+<h3 class ="course-title">${tags.title}</h3>` +
     Instructor_names(tags) +
     CountStars(tags) +
-    `</div><h5 class ="b">${tags.price} $</h5></div></div>`;
+    `</div>
+<h5 class ="course-price">${tags.price} $</h5>
+</div>`;
   return str;
+}
+function Carousel_Courses(Courses) {
+  let CarouselData = `
+  <div id="carouselExampleControls"    
+  data-interval="false" class="carousel slide" data-cycle-allow="true"  data-ride="carousel">
+  <div class="carousel-inner">
+`;
+
+  let NumberofCarousel = Math.ceil(Courses.length / pages);
+  for (let i = 0; i < NumberofCarousel; i++) {
+    CarouselData += `
+    <div id=${i} class="carousel-item ${currentIndex == i ? "active" : ""}">
+    <div class ="cards-wrapper">`;
+
+    for (let j = 0; j < pages && i * pages + j < Courses.length; j++) {
+      CarouselData += Courses[i * pages + j];
+    }
+    CarouselData += `</div></div>`;
+  }
+  CarouselData += `</div>
+  <a  class="carousel-control-prev"   href="#carouselExampleControls" role="button" data-slide="prev">
+  <span class="fa fa-chevron-left fa-lg"></span>
+</a >
+<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="fa fa-chevron-right fa-lg"></span>
+</a>
+  </div>`;
+  return CarouselData;
+}
+function Create_Course(tags) {
+  let str = "";
+  let Courses = [];
+  for (let i = 0; i < tags.courses.length; i++) {
+    Courses.push(Course_data(tags.courses[i]));
+    str += Course_data(tags.courses[i]);
+  }
+  return Carousel_Courses(Courses);
 }
 const filterCourses = async (e) => {
   e.preventDefault();
   let text = document.querySelector(".Search").value.toLowerCase();
-  let x = document.querySelector(".cour");
+  let x = document.querySelector(".course-info");
   let str = "";
-  for (c of Course) {
-    if (c.title.toLowerCase().includes(text)) str += Create_Course(c);
+  let C = [],
+    t = [];
+  for (let i = 0; i < tag.courses.length; i++) {
+    if (tag.courses[i].title.toLowerCase().includes(text)) {
+      str += Course_data(tag.courses[i]); // to return it withoud carousel
+      C.push(Course_data(tag.courses[i]));
+    }
+    t.push(Course_data(tag.courses[i]));
   }
-  x.innerHTML = str;
+  x.innerHTML = C.length == 0 ? Carousel_Courses(t) : Carousel_Courses(C);
 };
-const LoadCourses = async () => {
-  let url = "http://localhost:3000/courses";
+const LoadCourses = async (p) => {
+  let url = "http://localhost:3000/data";
   let fet = await fetch(url);
   let data = await fet.json();
-  Course = data;
-  let x = document.querySelector(".cour");
-  for (tags of data) {
-    x.innerHTML += Create_Course(tags);
-  }
+  const Res = data[p][0];
+  tag = data[p][0];
+  let x = document.querySelector(".course-type");
+  let str = `
+  <div class ="course-content">
+  <h2 class="Course-header" >${Res.header}</h2>
+  <p class="Course-description">${Res.description}</p>    
+  <button class = "explore-course">Explore ${p}</button>
+  <div class="course-info">`;
+  str += Create_Course(Res);
+  str += `</div></div>`;
+  x.innerHTML = str;
 };
+
+// e.target = item return from get element id
+let last = document.getElementById("c_python");
+document.getElementById("c_python").style.color = "black";
+
 function reload_Courses(e) {
+  currentIndex = 0;
+  document.querySelector(".Search").value = "";
   let x = e.target.innerHTML;
-  console.log(x);
+  def = x;
+  if (last != -1) last.style.color = "gray";
+  e.target.style.color = "black";
+  last = e.target;
+  LoadCourses(x);
 }
-LoadCourses();
+LoadCourses(def);
+let lst = -1;
+function Resize_Carousel_Courses() {
+  let width = this.window.innerWidth;
+  let text = document.querySelector(".Search").value.toLowerCase();
+  if (width >= 1400) {
+    pages = 5;
+  } else if (width < 1400 && width >= 1150) {
+    pages = 4;
+  } else if (width >= 850) {
+    pages = 3;
+  } else if (width >= 600) {
+    pages = 2;
+  } else if (width < 600) {
+    pages = 1;
+  }
+
+  let x = document.querySelector(".active");
+  currentIndex = x?.id ?? 0;
+  if (lst != pages) {
+    if (text == "") {
+      LoadCourses(def);
+    } else {
+      document.querySelector(".search-udemy-submit").click();
+    }
+    lst = pages;
+  }
+}
+window.addEventListener("resize", Resize_Carousel_Courses);
+Resize_Carousel_Courses();
 document
   .querySelector(".Search_form")
   .addEventListener("submit", filterCourses);
-
+LoadCourses(def);
 document.getElementById("coures").addEventListener("click", reload_Courses);
