9a6e70d3bd3aded8ba8be88a6e9ba08675973fa7
abdulrahman <abdelr7manfared@gmail.com>
2022-08-19 03:59:23 +0200

    refactor : Use guidelines for comments in JS

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 let tag;
 let cnt = 0;
-function Instructor_names(tags) {
+let last = document.getElementById("c_python");
+document.getElementById("c_python").style.color = "black";
+/**
+ * @description Get name of instructions
+ * @param {object} instructors - Data about instructors
+ * @returns{string}  Instructions nams
+ */
+function Instructor_names(instructors) {
   let str = "";
-  tags.instructors.forEach((element) => {
+  console.log(typeof instructors);
+  instructors.forEach((element) => {
     str += `<h4 class="instructor-name">${element.name}</h4>`;
   });
 
   return str;
 }
-function CountStars(tag) {
+/**
+ * @description Count The Stars based on rating
+ * @param {integer} Rate - Rate of Course
+ * @returns {string} Stars design and rate
+ */
+function CountStars(Rate) {
   let s = `<div> <span class="stars">4.4</span>`;
-  let Rate = tag.rating;
   for (let i = 1; i <= 5; i++) {
     if (Rate >= i) s += `<span class="fa fa-star stars"></span>`;
     else if (Rate + 0.5 >= i)
@@ -20,30 +32,44 @@ function CountStars(tag) {
   s += `</div>`;
   return s;
 }
-function Course_data(tags) {
+/**
+ * @description Make Course Component
+ * @param {object} Course - Contain all data about course
+ * @returns {string} Data about course
+ */
+function Course_data(Course) {
   let str =
     `
 <div class = "courses"> 
-<img class="course-image"src="${tags.image}"alt="pyhon"/>
+<img class="course-image"src="${Course.image}"alt="pyhon"/>
 <div class = "course-data">
-<h3 class ="course-title">${tags.title}</h3>` +
-    Instructor_names(tags) +
-    CountStars(tags) +
+<h3 class ="course-title">${Course.title}</h3>` +
+    Instructor_names(Course.instructors) +
+    CountStars(Course.rating) +
     `</div>
-<h5 class ="course-price">${tags.price} $</h5>
+<h5 class ="course-price">${Course.price} $</h5>
 </div>`;
   return str;
 }
-function Create_Course(tags) {
+/**
+ * @description Collect Courses
+ * @param {object} Course Contain all data about course
+ * @returns {string} Collect All Courses
+ */
+function Create_Course(Course) {
   let str = "";
-  tags.courses.forEach((element) => {
+  Course.forEach((element) => {
     str += Course_data(element);
   });
   return str;
 }
+/**
+ * @description Filter courses depend on course title
+ * @param {Event} e - Event clicked on search button
+ */
 const filterCourses = async (e) => {
   e.preventDefault();
-
+  console.log(typeof e);
   let text = document.querySelector(".Search").value.toLowerCase();
   let x = document.querySelector(".course-info");
   let str = "";
@@ -54,33 +80,36 @@ const filterCourses = async (e) => {
   });
   x.innerHTML = str;
 };
-const LoadCourses = async (p) => {
+/**
+ * @description Fetch api and retrive courses
+ * @param {string} Course_type - Type of Course
+ */
+const LoadCourses = async (Course_type) => {
   let url = "http://localhost:3000/data";
   let fet = await fetch(url);
   let data = await fet.json();
-  const Res = data[p][0];
-  tag = data[p][0];
+  tag = data[Course_type][0];
   let x = document.querySelector(".course-type");
   let str = `
   <div class ="course-content">
-  <h2 class="header" >${Res.header}</h2>
-  <p class="description">${Res.description}</p>    
-  <button class = "explore-course">Explore ${p}</button>
+  <h2 class="header" >${tag.header}</h2>
+  <p class="description">${tag.description}</p>    
+  <button class = "explore-course">Explore ${Course_type}</button>
   <div class="course-info">`;
-  str += Create_Course(Res);
+  str += Create_Course(data[Course_type][0].courses);
   str += `</div></div>`;
   x.innerHTML = str;
 };
-
-// e.target = item return from get element id
-let last = document.getElementById("c_python");
-document.getElementById("c_python").style.color = "black";
+/**
+ * @description Change data in Courses depend on Coursetype
+ * @param {Event} e - Event clicked on Type of course
+ */
 function reload_Courses(e) {
-  let x = e.target.innerHTML;
+  let Coursetype = e.target.innerHTML;
   if (last != -1) last.style.color = "gray";
   e.target.style.color = "black";
   last = e.target;
-  LoadCourses(x);
+  LoadCourses(Coursetype);
 }
 LoadCourses("Python");
 document
